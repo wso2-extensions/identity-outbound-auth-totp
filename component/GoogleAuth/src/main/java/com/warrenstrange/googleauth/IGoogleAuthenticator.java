e46aeed36d530c2fa7999aaded03b48e59217100
keetz <heykeetz@gmail.com>
2016-04-03 18:05:42 +0530

    Addidng GoogleAuth library

---
+++ component/GoogleAuth/src/main/java/com/warrenstrange/googleauth/IGoogleAuthenticator.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2014-2015 Enrico M. Crisostomo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   * Neither the name of the author nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.warrenstrange.googleauth;
+
+/**
+ * Google Authenticator library interface.
+ */
+@SuppressWarnings("UnusedDeclaration")
+public interface IGoogleAuthenticator {
+    /**
+     * This method generates a new set of credentials including:
+     * <ol>
+     * <li>Secret key.</li>
+     * <li>Validation code.</li>
+     * <li>A list of scratch codes.</li>
+     * </ol>
+     * <p/>
+     * The user must register this secret on their device.
+     *
+     * @return secret key
+     */
+    GoogleAuthenticatorKey createCredentials();
+
+    /**
+     * This method generates a new set of credentials invoking the
+     * <code>#createCredentials</code> method with no arguments. The generated
+     * credentials are then saved using the configured
+     * <code>#ICredentialRepository</code> service.
+     * <p/>
+     * The user must register this secret on their device.
+     *
+     * @param userName the user name.
+     * @return secret key
+     */
+    GoogleAuthenticatorKey createCredentials(String userName);
+
+    /**
+     * Checks a verification code against a secret key using the current time.
+     * The algorithm also checks in a time window whose size determined by the
+     * <code>windowSize</code> property of this class.
+     * <p/>
+     * The default value of 30 seconds recommended by RFC 6238 is used for the
+     * interval size.
+     *
+     * @param secret           the Base32 encoded secret key.
+     * @param verificationCode the verification code.
+     * @return <code>true</code> if the validation code is valid,
+     * <code>false</code> otherwise.
+     * @throws GoogleAuthenticatorException if a failure occurs during the
+     *                                      calculation of the validation code.
+     *                                      The only failures that should occur
+     *                                      are related with the cryptographic
+     *                                      functions provided by the JCE.
+     */
+    boolean authorize(String secret, int verificationCode)
+            throws GoogleAuthenticatorException;
+
+    /**
+     * This method validates a verification code of the specified user whose
+     * private key is retrieved from the configured credential repository. This
+     * method delegates the validation to the <code>#authorize</code> method.
+     *
+     * @param userName         The user whose verification code is to be
+     *                         validated.
+     * @param verificationCode The validation code.
+     * @return <code>true</code> if the validation code is valid,
+     * <code>false</code> otherwise.
+     * @throws GoogleAuthenticatorException if an unexpected error occurs.
+     * @see #authorize(String, int)
+     */
+    boolean authorizeUser(String userName, int verificationCode)
+            throws GoogleAuthenticatorException;
+}
