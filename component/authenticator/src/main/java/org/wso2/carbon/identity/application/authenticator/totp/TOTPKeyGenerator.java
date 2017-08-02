6f9838cf3d2c30f3f0218a4663c3de608a7d6a20
srajah.shakila@gmail.com <srajah.shakila@gmail.com>
2017-08-02 23:42:27 +0530

    Removing unwanted claims and changing the profiles accordingly

--- component/authenticator/src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPKeyGenerator.java
+++ component/authenticator/src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPKeyGenerator.java
@@ -124,6 +124,7 @@ public class TOTPKeyGenerator {
 			UserRealm userRealm = TOTPUtil.getUserRealm(username);
 			if (userRealm != null) {
 				tenantAwareUsername = MultitenantUtils.getTenantAwareUsername(username);
+				claims.remove(TOTPAuthenticatorConstants.QR_CODE_CLAIM_URL);
 				userRealm.getUserStoreManager().setUserClaimValues(tenantAwareUsername, claims, null);
 			}
 		} catch (UserStoreException e) {
