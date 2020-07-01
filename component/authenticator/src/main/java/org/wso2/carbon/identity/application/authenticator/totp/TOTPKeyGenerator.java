34acc7cfb494974762da27dcb1237fc0cf1b52ff
Chamath Samarawickrama <chamaths@wso2.com>
2020-07-01 09:35:05 +0530

    add timeStepSize as a param in QR code URL

--- component/authenticator/src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPKeyGenerator.java
+++ component/authenticator/src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPKeyGenerator.java
@@ -60,6 +60,7 @@ public class TOTPKeyGenerator {
 			UserRealm userRealm = TOTPUtil.getUserRealm(username);
 			String tenantDomain = MultitenantUtils.getTenantDomain(username);
 			tenantAwareUsername = MultitenantUtils.getTenantAwareUsername(username);
+			long timeStep = TOTPUtil.getTimeStepSize(context);
 			if (userRealm != null) {
 				Map<String, String> userClaimValues = userRealm.getUserStoreManager().
 						getUserClaimValues(tenantAwareUsername, new String[] {
@@ -82,7 +83,7 @@ public class TOTPKeyGenerator {
 
 				String issuer = TOTPUtil.getTOTPIssuerDisplayName(tenantDomain, context);
 				String qrCodeURL = "otpauth://totp/" + issuer + ":" + tenantAwareUsername + "?secret=" +
-						secretKey + "&issuer=" + issuer;
+						secretKey + "&issuer=" + issuer +"&period=" + timeStep;
 				encodedQRCodeURL = Base64.encodeBase64String(qrCodeURL.getBytes());
 				claims.put(TOTPAuthenticatorConstants.QR_CODE_CLAIM_URL, encodedQRCodeURL);
 			}
