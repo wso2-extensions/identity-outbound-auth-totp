7c94c124bb5abb664375a3ee20cf2a26ad500e5d
ayeshajay <ayeshajayasankha@gmail.com>
2021-07-12 12:10:20 +0530

    Refactore logs

--- component/authenticator/src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPAuthenticator.java
+++ component/authenticator/src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPAuthenticator.java
@@ -419,8 +419,9 @@ public class TOTPAuthenticator extends AbstractApplicationAuthenticator
 			String tenantDomain = context.getTenantDomain();
 			String sessionDataKey = context.getContextIdentifier();
 
-			String msg = "Sending verification code by email is disabled by admin. An attempt was made to send a " +
-					"verification code by email for user: %s for application: %s of %s tenant using sessionDataKey: %s";
+			String msg = "An attempt was made to send a verification code by email for user: %s " +
+					"for application: %s of %s tenant using sessionDataKey: %s. But sending verification code by " +
+					"email is disabled by admin.";
 			log.warn(String.format(msg, username, appName, tenantDomain, sessionDataKey));
 			return false;
 		}
