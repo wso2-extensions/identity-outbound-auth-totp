77f1d4893be30c134f181b28b3d3527306d10385
sadilchamishka <sandilchamishka@gmail.com>
2025-11-04 13:30:52 +0530

    Address comments

--- component/authenticator/src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPAuthenticator.java
+++ component/authenticator/src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPAuthenticator.java
@@ -194,11 +194,11 @@ public class TOTPAuthenticator extends AbstractApplicationAuthenticator
 							reason = errorCodeWithReason[1];
 						}
 					}
-                    Map<String, String> paramMap = new HashMap<>();
 
                     // Adds error information based on the error code type.
 					if (UserCoreConstants.ErrorCode.USER_IS_LOCKED.equals(errorCode)) {
-						paramMap.put(TOTPAuthenticatorConstants.ERROR_CODE, errorCode);
+                        Map<String, String> paramMap = new HashMap<>();
+                        paramMap.put(TOTPAuthenticatorConstants.ERROR_CODE, errorCode);
 						if (StringUtils.isNotBlank(reason)) {
 							paramMap.put(TOTPAuthenticatorConstants.LOCKED_REASON, reason);
 						}
@@ -209,12 +209,14 @@ public class TOTPAuthenticator extends AbstractApplicationAuthenticator
 							paramMap.put(TOTPAuthenticatorConstants.UNLOCK_TIME,
 									String.valueOf(Math.round((double) timeToUnlock / 1000 / 60)));
 						}
+                        errorParam = buildErrorParamString(paramMap);
 					} else if (UserCoreConstants.ErrorCode.INVALID_CREDENTIAL.equals(errorCode)) {
+                        Map<String, String> paramMap = new HashMap<>();
                         int remainingAttempts = Math.max(0, errorContext.getMaximumLoginAttempts() -
                                 errorContext.getFailedLoginAttempts());
                         paramMap.put(TOTPAuthenticatorConstants.REMAINING_ATTEMPTS, String.valueOf(remainingAttempts));
+                        errorParam = buildErrorParamString(paramMap);
                     }
-                    errorParam = buildErrorParamString(paramMap);
                 }
 			}
 
